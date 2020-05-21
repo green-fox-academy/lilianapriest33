5e19897d4b32ed63354247931c4d032432f18e7d
Viktoria Papp <lilianapriest33@gmail.com>
2020-05-21 17:09:21 +0200

    First commit

---
+++ lilianapriest33/week-01/day-4/HelloWorld/src/IntroduceYourself/IntroduceYourself.java
@@ -0,0 +1,17 @@
+package IntroduceYourself;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, My name is Viktoria Papp.");
+
+        System.out.println("I am");
+        System.out.println(32);
+        System.out.println("years old :)");
+
+        System.out.println("My height is");
+        System.out.println("1.68");
+        System.out.println("centimetres");
+
+    }
+}
