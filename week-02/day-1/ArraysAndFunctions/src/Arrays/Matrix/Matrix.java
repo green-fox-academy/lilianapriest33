685721f64ad7cee37549d6b3302a78097bf2d92d
Viktoria Papp <lilianapriest33@gmail.com>
2020-05-26 10:33:41 +0200

    Arrays exercises added

--- week-02/day-1/ArraysAndFunctions/src/Arrays/Matrix/Matrix.java
+++ week-02/day-1/ArraysAndFunctions/src/Arrays/Matrix/Matrix.java
@@ -1,6 +1,24 @@
 package Arrays.Matrix;
 
 public class Matrix {
+    public static void main(String[] args) {
+
+        int[][] map = new int[4][4];
+
+        int a = 0;
+        int b = 1;
+
+        for (int row = 0; row < map.length; row++) {
+            for (int col = 0; col < map.length; col++) {
+                if (row == col) {
+                    System.out.print(b + " ");
+                } else {
+                    System.out.print(a + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
 
 // - Create (dynamically) a two dimensional array
---
+++ week-02/day-1/ArraysAndFunctions/src/Arrays/ReverseList/ReverseList.java
@@ -0,0 +1,13 @@
+package Arrays.ReverseList;
+
+public class ReverseList {
+    public static void main(String[] args) {
+        int[] numbers = {3, 4, 5, 6, 7};
+
+        for (int i= numbers.length-1; i < numbers.length; i--)
+
+        System.out.println(numbers[i]);
+    }
+}
+// - Reverse the order of the elements in `numbers`
+// - Print the elements of the reversed `numbers`
