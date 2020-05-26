1fc672c876438d18f56cc6b1111c9fe1f7cb4dcf
Viktoria Papp <lilianapriest33@gmail.com>
2020-05-26 17:29:34 +0200

    new Strings exercises added

--- week-02/day-1/ArraysAndFunctions/src/Arrays/Colors/Colors.java
+++ week-02/day-1/ArraysAndFunctions/src/Arrays/Colors/Colors.java
@@ -1,5 +1,22 @@
+package Arrays.Colors;
 
+public class Colors {
+    public static void main(String[] args) {
 
+        String[] green = {"lime", "forest green", "olive", "pale green", "spring green"};
+        String[] red = {"orange red", "red", "tomato"};
+        String[] pink = {"orchid", "violet", "pink", "hot pink"};
+
+        String[][] colors = {green, red, pink};
+
+        for (int row = 0; row < colors.length; row++) {
+            for (int col = 0; col < colors[row].length; col++) {
+                System.out.print(colors[row][col] + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
 // - Create a two dimensional array
 //   which can contain the different shades of specified colors
 // - In `colors[0]` store the shades of green:
