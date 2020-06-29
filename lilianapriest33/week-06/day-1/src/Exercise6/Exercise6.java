3da45cecfd96898ddc24c5e366c62a7d33333078
Viktoria Papp <lilianapriest33@gmail.com>
2020-06-29 15:00:55 +0200

    Exercise 6 added

--- lilianapriest33/week-06/day-1/src/Exercise6/Exercise6.java
+++ lilianapriest33/week-06/day-1/src/Exercise6/Exercise6.java
@@ -1,2 +1,21 @@
-/*package Exercise6;
-*/
+package Exercise6;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.lang.Character.isUpperCase;
+
+public class Exercise6 {
+
+    public static void main(String[] args) {
+
+        String input = "GreenFoxAcademy";
+
+        List<Character> result = input.chars()
+                .filter(i -> isUpperCase(i))
+                .mapToObj(i -> (char) i)
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
+//Write a Stream Expression to find the uppercase characters in a string!
